36b4a03e9bc9cde2b37bec60e5234af9c07961f3
Mike Schultz <mike.schultz@gmail.com>
2025-06-04 10:08:13 -0400

    source .bashrc in update script

--- update_calendar.sh
+++ update_calendar.sh
@@ -5,6 +5,9 @@ set -e
 export HOME="/home/mike"
 export PATH="/usr/bin:/bin:/usr/local/bin:$HOME/.local/bin"
 
+# where passwords set
+source "$HOME/.bashrc"
+
 #Usage: scrape_vacasa.py <onlyFuture> <logPages> [<headless>]
 REPO_DIR="$HOME/2576-lwb"
 SCRAPE_COMMAND="$HOME/vacasa/venv/bin/python $HOME/vacasa/scrape_vacasa.py true false true"
